089979d9af1ebef1e8c363ae547a51697dcb478c
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-11-10 18:33:24 -0300

    Add Sales Amount and Pie Chart components

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,23 @@
-import './App.css';
-import Filter from './components/filter';
-import Header from './components/header';
+import "./App.css";
+import Filter from "./components/filter";
+import Header from "./components/header";
+import PieChart from "./components/pie-chart";
+import SalesAmount from "./components/sales-amount";
 
 function App() {
   return (
     <>
       <Header />
-      <div className='app-container'>
+      <div className="app-container">
         <Filter />
+        <div className="base-card app-sales-amount-container">
+          <SalesAmount />
+          <PieChart
+            name="Total de vendas"
+            labels={["Feminino", "Masculino", "Outro"]}
+            series={[40, 30, 30]}
+          />
+        </div>
       </div>
     </>
   );
---
+++ front-web/src/components/sales-amount/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+const SalesAmount = () => {
+    return (
+        <div className='sales-amount-container'>
+            <h1 className='sales-amount'>R$ 746.484,00</h1>
+            <h3 className='sales-amount-description'>Total de vendas</h3>
+        </div>
+    );
+}
+
+export default SalesAmount;
